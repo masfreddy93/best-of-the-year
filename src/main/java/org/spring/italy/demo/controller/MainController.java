cdb3ba98da520e5d611365a55f754d909c19c0fd
masfreddy93 <masieronicola@gmail.com>
2022-12-01 15:22:35 +0100

    first commit

---
+++ src/main/java/org/spring/italy/demo/controller/MainController.java
@@ -0,0 +1,23 @@
+package org.spring.italy.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+
+public class MainController {
+
+	@RequestMapping("/")
+	public String getHome(Model model) {
+		
+		String name = "Nicola";
+		
+		model.addAttribute("name", name);
+		
+		return "home";
+	}
+	
+	
+}
